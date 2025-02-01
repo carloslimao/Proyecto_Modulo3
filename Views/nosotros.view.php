e4c9f2fcbe8420154100ed518e98f577e2c92e3e
Carlos Lima <carlos.lima.orgaz@gmail.com>
2025-02-01 03:28:14 -0400

    Trabajo Final - Modulo 3

--- Views/nosotros.view.php
+++
@@ -1,3 +0,0 @@
-<?php require 'parciales/header.view.php'; ?>
-<h1>Nosotros</h1>
-<?php require 'parciales/footer.view.php'; ?>
--- Views/servicios.view.php
+++
@@ -1,3 +0,0 @@
-<?php require 'parciales/header.view.php'; ?>
-<h1>Servicios</h1>
-<?php require 'parciales/footer.view.php'; ?>
